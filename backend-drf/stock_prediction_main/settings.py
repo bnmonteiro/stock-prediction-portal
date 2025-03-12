ad725d8cd85b5aabb5ad68dbc3eb047be2259761
Bruno Monteiro <bruno.monteiro@dalfia.tech>
2025-03-12 20:43:32 -0300

    create register account endpoint

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'rest_framework',
+    'accounts',
+    'api'
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
